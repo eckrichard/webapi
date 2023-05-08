a02f175c648d6a3c5f0e3a58f5882b513cfb43e1
Eck Richárd <131364415+eckrichard@users.noreply.github.com>
2023-05-08 15:52:13 +0200

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using webapi.DAL;
+using webapi.Model;
+
+namespace webapi.Controllers
+{
+    // Keep the URL as-is
+    [Route("api/product")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        // Keep this contructor as-is
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
+        {
+            return productRepository.List();
+        }
+
+        // Add new api endpoints here
+        [HttpHead("{id}")]
+        public ActionResult<Model.Product> Head(int id)
+        {
+            var prod = productRepository.GetById(id);
+            if (prod == null)
+                return NotFound();
+            return prod;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Model.Product> Get(int id)
+        {
+            Product prod = productRepository.GetById(id);
+            if (prod == null)
+                return NotFound();
+            return prod;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Model.Product> Delete(int id)
+        {
+            var prod = productRepository.Delete(id);
+            if (prod)
+                return NoContent();
+            return NotFound();
+        }
+
+        [HttpGet("-/count")]
+        public CountResult Count()
+        {
+            var count = productRepository.List().Count;
+            CountResult countResult = new CountResult();
+            countResult.Count = count;
+            return countResult;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace webapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<DAL.IProductRepository, DAL.ProductRepository>();
+
+            // Add the OpenAPI/Swagger configuration here
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Add the OpenAPI/Swagger configuration here
+
+            app.UseRouting();
+            app.UseEndpoints(e => e.MapControllers());
+        }
+    }
+}
